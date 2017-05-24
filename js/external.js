36a8b37ca1ec6e9b25adea2abdc47df3765dcaa0
MilaSinica <ludmila.sinica@gmail.com>
2017-05-24 18:19:44 +0300

    szv website 1 version

---
+++ js/external.js
@@ -0,0 +1,16 @@
+
+  function openMe() {
+      x=document.getElementById("animation-vis");
+      x.style.display="none";
+      x=document.getElementById("animation-hid");
+      x.style.display="inline-block";
+
+  }
+
+
+
+
+
+
+
+
